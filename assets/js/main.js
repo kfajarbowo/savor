4a164813b2355eab2d248791a84fcef33c04dadf
kfajarbowo <kfajarbowo12@gmail.com>
2021-09-15 14:07:04 +0700

    add : add function scroll header & scroll to top

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,10 +36,27 @@ function scrollActive(){
         sectionId = current.getAttribute('id')
         
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
         }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
     })
 }
 window.addEventListener('scroll', scrollActive)
+
+// Change Background Header
+
+function scrollHeader(){
+    const nav = document.getElementById('header')
+
+    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+// Show ScrollTop
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top')
+    //ketika scroll lebih tinggi dari 560 , add a class show-scroll
+    if(this.scrollY >= 560) scrollTop.classList.add('scroll-top'); else scroll.classList.remove('scroll-top')
+}
+window.addEventListener('scroll', scrollTop)
